2d440379b0119afcbdb0e49cbf10d32695030eea
Meilin Li (Linda) <limeilinda@gmail.com>
2022-08-12 13:14:06 -0400

    Added annotations to MuffinRepo and BakerRepo and BakerController and MuffinControler

--- src/main/java/com/zipcodewilmington/bakery/services/BakerService.java
+++ src/main/java/com/zipcodewilmington/bakery/services/BakerService.java
@@ -2,10 +2,12 @@ package com.zipcodewilmington.bakery.services;
 
 import com.zipcodewilmington.bakery.models.Baker;
 import com.zipcodewilmington.bakery.repositories.BakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BakerService {
+
     private BakerRepository repository;
 
     public BakerService(BakerRepository repository) {
