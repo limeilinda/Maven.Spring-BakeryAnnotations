4e213cfcc5e7235ced9bd792f01aeed50a483a93
Meilin Li (Linda) <limeilinda@gmail.com>
2022-08-14 12:28:53 -0400

    Added annotations to Baker and Muffin classes

--- src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
@@ -2,12 +2,16 @@ package com.zipcodewilmington.bakery.controllers;
 
 import com.zipcodewilmington.bakery.models.Muffin;
 import com.zipcodewilmington.bakery.services.MuffinService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody
 public class MuffinController {
+    @Autowired
     private MuffinService service;
 
     public MuffinController(MuffinService service) {
